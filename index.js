f17b46eb94d98cecf5a8de2b0d0f0b10994ea130
paoloswap <122459630+paoloswap@users.noreply.github.com>
2024-08-05 18:55:54 +0200

    Migrate telos subgraphs to 0xgraph.xyz (ormi)

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ const SUBGRAPH_URLS = {
   [SUBGRAPH_NAMES.ANALYTICS]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/analytics/2024-07-11/gn",
     [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/analytics-testnet/-/gn",
-    [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/analytics",
+    [ChainId.TELOS]: "https://telosapi.0xgraph.xyz/api/public/f59149ee-c99a-41d0-afe4-1c86170a98b0/subgraphs/cryptoalgebra/analytics/2024-08-05/gn",
     [ChainId.TELOS_TESTNET]: "https://testnet.telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/analytics",
   },
   [SUBGRAPH_NAMES.FARMS]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/farms/-/gn",
     [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/farms-testnet/-/gn",
-    [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/farms2",
+    [ChainId.TELOS]: "https://telosapi.0xgraph.xyz/api/public/f59149ee-c99a-41d0-afe4-1c86170a98b0/subgraphs/cryptoalgebra/farms2/2024-08-05/gn",
     [ChainId.TELOS_TESTNET]: "https://testnet.telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/farms",
   },
   [SUBGRAPH_NAMES.BLOCKS]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/blocks/-/gn",
     [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/blocks-testnet/-/gn",
-    [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/blocks2",
+    [ChainId.TELOS]: "https://telosapi.0xgraph.xyz/api/public/f59149ee-c99a-41d0-afe4-1c86170a98b0/subgraphs/cryptoalgebra/blocks/2024-08-05/gn",
     [ChainId.TELOS_TESTNET]: "https://testnet.telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/blocks",
   },
   [SUBGRAPH_NAMES.ICE_TOKEN]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/subgraphs/name/swapsicle/icetoken",
     [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/subgraphs/name/swapsicle/icetoken-testnet",
-    [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/swapsicle/ice-token",
+    [ChainId.TELOS]: "https://telosapi.0xgraph.xyz/api/public/f59149ee-c99a-41d0-afe4-1c86170a98b0/subgraphs/swapsicle/ice-token/2024-08-04/gn",
     [ChainId.TELOS_TESTNET]: "https://api.goldsky.com/api/public/project_clr6mlufzbtuy01vd012wgt5k/subgraphs/swapsicle/ice-token-testnet/gn",
   },
 }
