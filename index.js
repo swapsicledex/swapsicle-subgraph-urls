4e59b1f1f200544104575f7fe212004cda418764
paoloswap <122459630+paoloswap@users.noreply.github.com>
2024-08-27 09:51:40 +0200

    Update Mantle Testnet URLs

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ const SUBGRAPH_NAMES = {
 const SUBGRAPH_URLS = {
   [SUBGRAPH_NAMES.ANALYTICS]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/analytics/2024-07-11/gn",
-    [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/analytics-testnet/-/gn",
+    [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/swapsicle/analytics-testnet/2024-08-27/gn",
     [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/analytics",
     [ChainId.TELOS_TESTNET]: "https://testnet.telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/analytics",
   },
   [SUBGRAPH_NAMES.FARMS]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/farms/-/gn",
-    [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/farms-testnet/-/gn",
+    [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/swapsicle/farms-testnet/2024-08-27/gn",
     [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/farms2",
     [ChainId.TELOS_TESTNET]: "https://testnet.telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/farms",
   },
   [SUBGRAPH_NAMES.BLOCKS]: {
     [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/blocks/-/gn",
-    [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/blocks-testnet/-/gn",
+    [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/swapsicle/blocks-testnet/2024-08-27/gn",
     [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/blocks2",
     [ChainId.TELOS_TESTNET]: "https://testnet.telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/blocks",
   },
