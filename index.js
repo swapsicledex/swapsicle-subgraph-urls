a6a788a7682b65a44c3136b31a9404c762c7ac0a
paoloswap <122459630+paoloswap@users.noreply.github.com>
2024-09-06 18:27:32 +0200

    Restore old (working) farms Mantle subgraph URL

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const SUBGRAPH_URLS = {
     [ChainId.TAIKO_TESTNET]: "https://api.goldsky.com/api/public/project_clr6mlufzbtuy01vd012wgt5k/subgraphs/swapsicle-analytics-taiko-hekla/prod/gn",
   },
   [SUBGRAPH_NAMES.FARMS]: {
-    [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/swapsicle/farms/prod/gn",
+    [ChainId.MANTLE]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/cryptoalgebra/farms/-/gn",
     [ChainId.MANTLE_TESTNET]: "https://subgraph-api.mantle.xyz/api/public/f077c8d4-0d6c-42d4-9bbd-050948dc5c86/subgraphs/swapsicle/farms-testnet/prod/gn",
     [ChainId.TELOS]: "https://telos.subgraph.swapsicle.io/subgraphs/name/cryptoalgebra/farms2",
     [ChainId.TELOS_TESTNET]: "https://telosapi.0xgraph.xyz/api/public/f59149ee-c99a-41d0-afe4-1c86170a98b0/subgraphs/swapsicle/farms-testnet/prod/gn",
